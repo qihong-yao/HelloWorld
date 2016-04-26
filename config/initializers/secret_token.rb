7ba5823f4901e4ad016599e6a2a0b1b743689f7a
Qihong yao <qihong.yao@spirent.com>
2016-04-26 13:34:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningRails::Application.config.secret_token = 'c1ad310afe2948fe4457f97998670a8df7ca83d6c2c5272c365a68b65fcd9ad2896d65a97748f658b6b1ad8b634ec550eaef68c0843b30834cc04c0a150f7556'
